af7ece87722c17c6988658f2530beb353ac2c8d5
drawde <drawde@drawde>
2015-03-01 03:44:12 -0500

    creacion de los formulario clientes y prospectos y mostrandos los clientes en tablas

---
+++ public/js/business.js
@@ -0,0 +1,43 @@
+$(document).on('ready', function() {
+    $('.type1').removeClass('hidden');
+    $('.type-tow2').removeClass('hidden');
+    var i= 0,j=0;
+    $('.business-fixed').on('click',function()
+   {
+       if(i==0)
+       {
+           $('.business-one').removeClass('hidden');
+           $('.business-two').addClass('hidden');
+           $('.business1').val(1);
+           $('.business2').val(0);
+           i++;
+           j=0;
+       }else{
+           $('.business-one').addClass('hidden');
+           $('.business1').val(0);
+           i=0;
+       }
+
+
+
+   });
+
+    $('.business').on('click',function()
+    {
+        if(j==0)
+        {
+            $('.business-two').removeClass('hidden');
+            $('.business-one').addClass('hidden');
+            $('.business2').val(2);
+            $('.business1').val(0);
+            j++;
+            i=0;
+        }else{
+            $('.business-two').addClass('hidden');
+            $('.business2').val(0);
+            j=0;
+        }
+    });
+
+
+});
