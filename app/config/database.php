ca18877ab3afe90040f5c7ec42700897905678c8
chiricos <edwarddiaz92@gmail.com>
2015-04-22 23:00:31 -0500

    probando

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'cerverus',
+			'database'  => 'cerverusdfas',
 			'username'  => 'root',
 			'password'  => '123456',
 			'charset'   => 'utf8',
